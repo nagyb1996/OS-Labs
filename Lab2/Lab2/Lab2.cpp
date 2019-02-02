37988f41624c66b77f6e316d2e389f79e5a402cf
nagyb1996 <nagyb@wit.edu>
2019-02-02 17:56:25 -0500

    New Methods

--- Lab2/Lab2/Lab2.cpp
+++ Lab2/Lab2/Lab2.cpp
@@ -9,42 +9,60 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <array>
 
 using namespace std;
 
-vector<string> split(string content, string delim);
+vector<string> split(string content, string delim); //declare split, used to split string based on arguement defined delim
 
-void instruction_parser(string instruct, map<string, string> dic);
+void instruction_parser(string instruct, map<string, string> dic); //declare instruction_parser, parses instructions and updates map
+
+
+const string instructionCommands[8] = { "dispatched", "requests", "swapped in", "swapped out", "interrupt", " terminated", "created", "expires" };
+
+
+/*
+dispatched -> running
+requests -> blocked
+
+
+*/
 
 
 
 void instruction_parser(string instruct, map<string, string> dic) {
 
-	vector<string> instructVector;
+	vector<string> instructVector;			//new string vector, holds supplied instructions
 
-	instructVector.push_back(instruct);
+	instructVector.push_back(instruct);		//add instruction to vector, if there is more than instruction, if statement will catch and split it
 
-	if (instruct.find(";") != string::npos) {
+	if (instruct.find(";") != string::npos) {	//splits line of mulitple instructions into multiple strings
 
 		instructVector = split(instruct, ";");
 
 	}
 
 
-	for (std::size_t i = 0; i < instructVector.size(); i++) {
+	for (std::size_t i = 0; i < instructVector.size(); i++) { 
 
-		string node;
+		string process;
+		string state;
 
 		map<string, string>::iterator it1;
-		for (it1 = dic.begin(); it1 != dic.end(); ++it1) {
+		for (it1 = dic.begin(); it1 != dic.end(); ++it1) { //find the key that the instruction will apply to
 			if (instructVector[i].find(it1->first) != string::npos) {
-				node = it1->first;
+				process = it1->first;
 			}
 		}
 
+		for (int j = 0; j < 8; j++) {
+			if (instructVector[i].find(instructionCommands[j]) != string::npos) {
+				state = instructionCommands[j];
+			}
+		}
 
-
-
+		//update this later when we know logic
+		dic[process] = state;
 
 	}
 
@@ -75,13 +93,14 @@ vector<string> split(string content, string delim) {
 	int position = 0;
 	string token;
 
-	while ((position = content.find(delim)) != string::npos) {
+	while ((position = content.find(delim)) != string::npos) { //split strings based on supplied delim token
 		token = content.substr(0, position);
 		inits.push_back(token);
 		content.erase(0, position + delim.length());
 	}
-	inits.push_back(content); //Add watever is left, with no delimeters
+	inits.push_back(content); //Add whatever is left, with no delimeters, fixes issue of dropping final state
 
+	//print debugging tool
 	// for (int i = 0; i != inits.size(); i++) {
 	//	cout << inits[i] << endl;
 	//}
@@ -130,9 +149,12 @@ int main() {
 
 	}
 
-	map<string, string>::iterator it1;
+	//send each individual line to the line parser
 
-	for (it1 = stateMap.begin(); it1 != stateMap.end(); ++it1)
+
+	map<string, string>::iterator it1;
+	//map print debugging tool
+	for (it1 = stateMap.begin(); it1 != stateMap.end(); ++it1) 
 		cout << it1->first << "->" << it1->second << endl;
 
 
